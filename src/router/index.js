d0104179863d9e0454fdc41a3ca56fbb7eccdf91
Nomta <nomta@outlook.com>
2022-12-30 07:56:21 +0300

    router

--- src/router/index.js
+++ src/router/index.js
@@ -9,13 +9,29 @@ const routes = [
   {
     path: '/books',
     name: 'books',
-    component: () => import(/* webpackChunkName: "Books" */ '../views/BooksView.vue')
+    component: () => import(/* webpackChunkName: "Books" */ '@/views/BooksView.vue')
   },
   {
-    path: '/books/:bookId',
+    path: '/books/create',
+    name: 'createBook',
+    component: () => import(/* webpackChunkName: "Books" */ '@/views/BookEditorView.vue')
+  },
+  {
+    path: '/books/:id',
     name: 'book',
-    component: () => import(/* webpackChunkName: "BookDetail" */ '../views/BookDetailView.vue'),
+    component: () => import(/* webpackChunkName: "BookDetail" */ '@/views/BookDetailView.vue'),
     props: true,
+  },
+  {
+    path: '/books/:id/edit',
+    name: 'editBook',
+    component: () => import(/* webpackChunkName: "BookDetail" */ '@/views/BookEditorView.vue'),
+    props: true,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'notFound',
+    component: () => import(/* webpackChunkName: "NotFound" */ '@/views/NotFound.vue')
   },
 ]
 
